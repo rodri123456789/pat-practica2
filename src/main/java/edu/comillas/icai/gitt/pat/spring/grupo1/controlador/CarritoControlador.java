8d1e9e2f37bf896da091b91aa1aebd54a2d9839e
Rodrigo <202312541@alu.comillas.edu>
2026-02-08 19:01:39 +0100

    segundo commit

---
+++ src/main/java/edu/comillas/icai/gitt/pat/spring/grupo1/controlador/CarritoControlador.java
@@ -0,0 +1,4 @@
+package edu.comillas.icai.gitt.pat.spring.grupo1.controlador;
+
+public class CarritoControlador {
+}
---
+++ src/main/java/edu/comillas/icai/gitt/pat/spring/grupo1/modelo/Carrito.java
@@ -0,0 +1,4 @@
+package edu.comillas.icai.gitt.pat.spring.grupo1.modelo;
+
+public class Carrito {
+}
