d79eed903a2cb4bed05ab60e12f42c14a4f9205c
Rodrigo <202312541@alu.comillas.edu>
2026-02-08 17:46:10 +0100

    first commit

---
+++ src/main/java/edu/comillas/icai/gitt/pat/spring/grupo1/Grupo1Application.java
@@ -0,0 +1,13 @@
+package edu.comillas.icai.gitt.pat.spring.grupo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Grupo1Application.class, args);
+	}
+
+}
