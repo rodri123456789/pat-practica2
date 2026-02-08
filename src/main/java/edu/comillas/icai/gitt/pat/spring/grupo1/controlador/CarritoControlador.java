1431ff1e6468dde083fdc8d7f7bc406185ea8036
Rodrigo <202312541@alu.comillas.edu>
2026-02-08 20:14:29 +0100

    añadido put

--- src/main/java/edu/comillas/icai/gitt/pat/spring/grupo1/controlador/CarritoControlador.java
+++ src/main/java/edu/comillas/icai/gitt/pat/spring/grupo1/controlador/CarritoControlador.java
@@ -1,4 +1,56 @@
 package edu.comillas.icai.gitt.pat.spring.grupo1.controlador;
 
+import edu.comillas.icai.gitt.pat.spring.grupo1.modelo.Carrito;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
 public class CarritoControlador {
+    private final Map<Integer, Carrito> carritos = new HashMap<>();
+
+    @GetMapping("/api/carrito")
+    public Collection<Carrito> getCarritos() {
+
+        return carritos.values();
+    }
+
+    @PostMapping("/api/carrito")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Carrito creaCarrito(@RequestBody Carrito carrito) {
+        carritos.put(carrito.getIdCarrito(), carrito);
+        return carrito;
+        }
+
+    @GetMapping("/api/carrito/{idCarrito}")
+    public Carrito getCarrito(@PathVariable int idCarrito) {
+
+        return carritos.get(idCarrito);
+    }
+
+    @DeleteMapping("/api/carrito/{idCarrito}")
+    public void borrarCarrito(@PathVariable int idCarrito) {
+
+        carritos.remove(idCarrito);
+    }
+
+    @PutMapping("/api/carrito/{idCarrito}")
+    public Carrito modificarCarrito(@PathVariable int idCarrito, @RequestBody Carrito carrito){
+        carritos.put(idCarrito, carrito);
+        return carrito;
+    }
+//
+//    @PutMapping("/api/contadores/{nombre}/incremento/{incremento}")
+//    public ModeloContador incrementa(@PathVariable String nombre,
+//                                     @PathVariable Integer incremento) {
+//        ModeloContador contadorActual = contadores.get(nombre);
+//        ModeloContador contadorIncrementado =
+//                new ModeloContador(nombre, contadorActual.valor() + incremento);
+//        contadores.put(nombre, contadorIncrementado);
+//        return contadorIncrementado;
+//    }
 }
+
--- src/main/java/edu/comillas/icai/gitt/pat/spring/grupo1/modelo/Carrito.java
+++ src/main/java/edu/comillas/icai/gitt/pat/spring/grupo1/modelo/Carrito.java
@@ -1,4 +1,62 @@
 package edu.comillas.icai.gitt.pat.spring.grupo1.modelo;
 
 public class Carrito {
+
+    private int idCarrito;
+
+    private int idArticulo;
+
+    private String descripcion;
+
+    private int unidades;
+
+    private double precioFinal;
+
+    public Carrito(int idCarrito, int idArticulo, String descripcion, int unidades, double precioFinal) {
+        this.idCarrito = idCarrito;
+        this.idArticulo = idArticulo;
+        this.descripcion = descripcion;
+        this.unidades = unidades;
+        this.precioFinal = precioFinal;
+    }
+
+    public int getIdCarrito() {
+        return idCarrito;
+    }
+
+    public void setIdCarrito(int idCarrito) {
+        this.idCarrito = idCarrito;
+    }
+
+    public int getIdArticulo() {
+        return idArticulo;
+    }
+
+    public void setIdArticulo(int idArticulo) {
+        this.idArticulo = idArticulo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getUnidades() {
+        return unidades;
+    }
+
+    public void setUnidades(int unidades) {
+        this.unidades = unidades;
+    }
+
+    public double getPrecioFinal() {
+        return precioFinal;
+    }
+
+    public void setPrecioFinal(double precioFinal) {
+        this.precioFinal = precioFinal;
+    }
 }
